d4e1d03c4bc6601ca96f94c2b440748a0baf7271
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-02-26 15:22:15 -0500

    seed

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,4 +1,4 @@
-class Media < ApplicationRecord
+class Medium < ApplicationRecord
   belongs_to :influencer
   belongs_to :format
   belongs_to :platform
--- db/migrate/20190225212832_create_states.rb
+++ db/migrate/20190225212832_create_states.rb
@@ -1,10 +1,10 @@
 class CreateStates < ActiveRecord::Migration[5.2]
   def change
     create_table :states do |t|
-      t.boolean :read
-      t.integer :display_count
+      t.boolean :read, default: false
+      t.integer :display_count, default: 0
       t.references :user, foreign_key: true
-      t.references :media, foreign_key: true
+      t.references :medium, foreign_key: true
 
       t.timestamps
     end
