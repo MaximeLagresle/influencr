1ae4c02e83f3e43ab183cd1c15e9de7028b40e28
Gary Franks <gary.udle@gmail.com>
2019-02-27 18:13:37 -0500

    capture session time

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,8 @@
 import "bootstrap";
 
-// ANIMATION FOR BUBBLE OPEN/CLOSE
-// WILL MOVE TO SEPERAETE JS FILE ONCE WORKING
+// Convert last login to current timezone
+// Will refactor later
 
-
-// $(".bubble-close").click(function () {
-
-//     $close = $(this);
-//     //getting the next element
-//     $open = $close.next();
-//     //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-//     $open.slideToggle(400, function () {
-//         //execute this after slideToggle is done
-//         //change text of header based on visibility of content div
-//         $close.text(function () {
-//             //change text based on condition
-//             return $open.is(":visible") ? "Opened" : "Closed";
-//         });
-//     });
-
-// });
+const lastLogin = document.querySelector("#utc")
+const adjustedTime = new Date(Date.parse(lastLogin.dataset.time))
+lastLogin.innerHTML = adjustedTime.toLocaleString()
