f5dd7cf3bbc1a51abbbc94d5df2b087aa0af3c56
Edouard Capel <edouard.capel@gmail.com>
2019-02-27 18:40:44 -0500

    SlideBubble

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,57 @@
 import "bootstrap";
 
+const bubbles = document.querySelectorAll(".bubble")
+bubbles.forEach( bubble => {
+  bubble.addEventListener('click', (e) => {
 
-$(".bubble").click(function () {
-    console.log("test")
-    console.log($(this))
+    const content = bubble.querySelector(".content");
+    // console.log(content.offsetHeight);
+    bubble.classList.toggle("extend");
+    // content.classList.toggle("hidden");
 
-    $(this).toggleClass("bl-open")
-    $(this).toggleClass("bl-show-text")
+    e.target.style.height =  `${e.target.offsetHeight + (content.offsetHeight+50)}px`
+    bubble.addEventListener("transitionend", (event) => {
+      content.classList.toggle("hidden");
+  })
+ })
+})
+
+
+    // e.target.style.height =  `${e.target.offsetHeight + content.offsetHeight}px`
+    // console.log(e.target.offsetHeight + 76);
+
+
+
+// jQuery.fn.animateAuto = function(prop, speed, callback){
+//     var elem, height, width;
+//     return this.each(function(i, el){
+//         el = jQuery(el), elem = el.clone().css({"height":"auto","width":"auto"}).appendTo("body");
+//         height = elem.css("height"),
+//         width = elem.css("width"),
+//         elem.remove();
+
+//         if(prop === "height")
+//             el.animate({"height":height}, speed, callback);
+//         else if(prop === "width")
+//             el.animate({"width":width}, speed, callback);
+//         else if(prop === "both")
+//             el.animate({"width":width,"height":height}, speed, callback);
+//     });
+// }
+
+
+
+// $(".bubble").click(function () {
+//   console.log($("p.content"))
+//   $("p.content").toggleClass("hidden")
+  // $(this).animateAuto("height", 100);
+    // console.log("test")
+    // console.log($(this))
+
+
+
+    // sleep(2000);
+    // $(this).toggleClass("bl-show-text")
 
 
 
@@ -23,4 +68,4 @@ $(".bubble").click(function () {
     //     });
     // });
 
-});
+// });
