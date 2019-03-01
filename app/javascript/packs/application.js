96228de98462fa69ffe70df95fdd705ef37734db
Gary Franks <gary.udle@gmail.com>
2019-03-01 13:26:34 -0500

    started right-side bubble

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,26 +1,27 @@
 import "bootstrap";
 
 
-const bubbles = document.querySelectorAll(".bubble")
+const bubbles = document.querySelectorAll(".content")
+
 bubbles.forEach( bubble => {
 
-  // bubble.addEventListener('mouseover', (e) => {
-
+  bubble.addEventListener("click", (e) => {
   //   bubble.classList.toggle("open");
-  //   const content = bubble.querySelector(".content");
+    const content = bubble.querySelector(".bubble-right");
 
   //   bubble.addEventListener("transitionend", (event) => {
   //     content.classList.toggle("hidden");
   //   })
 
-  // })
+    console.log("hello")
+  })
 
-})
+});
 
 // Convert last login to current timezone
 // Will refactor later
 
-const lastLogin = document.querySelector("#utc")
-const adjustedTime = new Date(Date.parse(lastLogin.dataset.time))
-lastLogin.innerHTML = adjustedTime.toLocaleString()
+// const lastLogin = document.querySelector("#utc")
+// const adjustedTime = new Date(Date.parse(lastLogin.dataset.time))
+// lastLogin.innerHTML = adjustedTime.toLocaleString()
 
