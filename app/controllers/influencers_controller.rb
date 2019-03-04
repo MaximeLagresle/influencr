c13d03dff7aa1f59c6fce3c449f337f1cb4012ee
Gary Franks <gary.udle@gmail.com>
2019-03-04 16:54:35 -0500

    added dynamic article links to the influencer show modal

--- app/controllers/influencers_controller.rb
+++ app/controllers/influencers_controller.rb
@@ -5,7 +5,18 @@ class InfluencersController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to root_path }
-      format.js # <-- will render `app/views/reviews/create.js.erb`
+      format.js
+    end
+  end
+
+  def collect_media
+    @influencer = Influencer.find(params[:influencer_id])
+    @format = Format.find_by(type_of: params[:formata])
+    @media = @influencer.media.where(format: @format)
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
     end
   end
 end
