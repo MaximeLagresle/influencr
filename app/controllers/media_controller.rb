ee26ee9cf8df6674e19dd06486b582edb8392ab9
Maxime Lagresle <maximelagresle@hotmail.fr>
2019-03-07 15:48:45 -0500

    onboarding done

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,57 +1,62 @@
 class MediaController < ApplicationController
   def index
+    if current_user.first_onboarding == true
+      current_user.first_onboarding = false
+      current_user.save
+      redirect_to tutorial_path
+    else
+      @media = Medium.all
+      @influencers = Influencer.all
 
-    @media = Medium.all
-    @influencers = Influencer.all
+      @preferences = Preference.where(user: current_user)
 
-    @preferences = Preference.where(user: current_user)
+    # checks to see if a state record exists for each medium. creates one if false.
 
-  # checks to see if a state record exists for each medium. creates one if false.
-
-    @media.each do |medium|
-      create_state(medium) unless validate_state?(medium)
-    end
-
-  # store all the current user's state records. sorts by the algorithm in the state model.
-
-    @state = current_user.states.sort_by(&:algie_check).reverse
-
-  # store the ten most relevent state records. send to the view and increment display_count.
-    s_array = []
-    tweet_array = []
-
-    @state.each do |s|
-      if s.medium.platform.name != "Twitter"
-        s_array << s
-      else
-        tweet_array << s
+      @media.each do |medium|
+        create_state(medium) unless validate_state?(medium)
       end
+
+    # store all the current user's state records. sorts by the algorithm in the state model.
+
+      @state = current_user.states.sort_by(&:algie_check).reverse
+
+    # store the ten most relevent state records. send to the view and increment display_count.
+      s_array = []
+      tweet_array = []
+
+      @state.each do |s|
+        if s.medium.platform.name != "Twitter"
+          s_array << s
+        else
+          tweet_array << s
+        end
+      end
+
+      insert_tweet = tweet_array.first(3)
+      insert_array = s_array.first(9)
+
+      insert_tweet.each do |t|
+        insert_array << t
+      end
+
+      @state_array = insert_array.map { |state| increment_state!(state.medium) }
+
+      # defaults to the first influencer's media for filter window
+
+      @influencer = Influencer.first if @influencer.nil?
+
+      # define first preference to show add or delete option
+
+      @preference = Preference.find_by(user: current_user, influencer: @influencer)
+
+      # set default format to article, display the influencer's articles only
+
+      form = Format.find_by(type_of: "article")
+      @media = @influencer.media.where(format: form)
+
+      #Active Record alternative for faster processing.
+      # @influencer.media.joins(:format).where(formats: { type_of: "article" } ).count
     end
-
-    insert_tweet = tweet_array.first(3)
-    insert_array = s_array.first(9)
-
-    insert_tweet.each do |t|
-      insert_array << t
-    end
-
-    @state_array = insert_array.map { |state| increment_state!(state.medium) }
-
-    # defaults to the first influencer's media for filter window
-
-    @influencer = Influencer.first if @influencer.nil?
-
-    # define first preference to show add or delete option
-
-    @preference = Preference.find_by(user: current_user, influencer: @influencer)
-
-    # set default format to article, display the influencer's articles only
-
-    form = Format.find_by(type_of: "article")
-    @media = @influencer.media.where(format: form)
-
-    #Active Record alternative for faster processing.
-    # @influencer.media.joins(:format).where(formats: { type_of: "article" } ).count
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_223651) do
+ActiveRecord::Schema.define(version: 2019_03_07_182309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2019_03_04_223651) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.boolean "first_onboarding", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
