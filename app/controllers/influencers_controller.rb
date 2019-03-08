4a294265f82bbcbf5fd7c6542cc39a8863d0bd2c
Gary Franks <gary.udle@gmail.com>
2019-03-08 12:58:13 -0500

    added fixes for various presentation priorities

--- app/controllers/influencers_controller.rb
+++ app/controllers/influencers_controller.rb
@@ -15,6 +15,9 @@ class InfluencersController < ApplicationController
     @all_my_states = State.where(user: current_user)
     @my_states = @all_my_states.select { |s| @my_influencers.include?(s.medium.influencer) }
 
+    @my_states.map! { |state| increment_state!(state.medium) }
+    sort_by_algo(@my_states)
+
     @preference = Preference.find_by(user: current_user, influencer: @influencer)
 
     form = Format.find_by(type_of: "article")
@@ -35,5 +38,20 @@ class InfluencersController < ApplicationController
       format.html { redirect_to root_path }
       format.js
     end
+  end
+
+  # searches for a state record for each medium associated to the current user
+  def find_state(medium)
+    State.find_by(medium: medium, user: current_user)
+  end
+
+  # sorts the list with algorithm - in state model
+  def sort_by_algo(state_list)
+    state_list.sort_by(&:algie_check)
+  end
+
+  # increases the display count of the state record by 1
+  def increment_state!(medium)
+    find_state(medium).increment!(:display_count)
   end
 end
